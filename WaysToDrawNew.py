708f0a4c2c543ad45c17a94100bc3f1dd1b429fa
truthmane <143239597+truthmane@users.noreply.github.com>
2023-08-28 18:51:34 -0400

    Add files via upload

--- WaysToDrawNew.py
+++ WaysToDrawNew.py
@@ -1,23 +1,23 @@
-import math
+import math, argparse
 from rackFunctions import ways_to_draw, sum_from_spot, print_stats
 number_of_lines = 22000
 draws = []
-number_of_alphagrams=int(input("Number of alphagrams: "))
-starting_alphagram=int(input("Starting alphagram: "))-1
+parser = argparse.ArgumentParser()
+parser.add_argument("quant", type=int, help="Enter the number of alphagrams you want analyzed")
+parser.add_argument("start", type=int, help="Enter the probability of the alphagram you wish to start with")
+args = vars(parser.parse_args())
+number_of_alphagrams = args['quant']
+starting_alphagram = args['start']
 
 with open("/Users/etbrosowsky/Zyzzyva/words/saved/SevenAlphagrams.txt") as SevenAlphagrams:
-    alphagrams_list = []
     counter = 0
     for line in SevenAlphagrams:
-        alphagrams_list.append(line.rstrip())
+        draws.append(round(ways_to_draw(line.rstrip())))
         counter += 1
         if counter == number_of_lines:
             break
 
-for rack in alphagrams_list:
-    draws.append(round(ways_to_draw(rack)))
-
 draws.sort(reverse=True)
 total_draws = sum(draws)
 alphagram_sum = sum_from_spot(draws, number_of_alphagrams, starting_alphagram)
-print_stats(draws, total_draws, number_of_alphagrams, alphagram_sum)
+print_stats(draws, total_draws, number_of_alphagrams, alphagram_sum, starting_alphagram)
--- rackFunctions.py
+++ rackFunctions.py
@@ -4,7 +4,7 @@ frequencies=[9,2,2,4,12,2,3,4,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1]
 def ways_to_draw(rack):
     ways_quantity=1
     for letter in rack:
-        ways_quantity*=math.comb(frequencies[ord(letter) - 65],rack.count(letter))**(1/rack.count(letter))
+        ways_quantity*=int(math.comb(frequencies[ord(letter) - 65],rack.count(letter))**(1/rack.count(letter)))
     return(ways_quantity)
 
 def sum_from_spot(list, length, start):
@@ -13,7 +13,8 @@ def sum_from_spot(list, length, start):
     sum += list[i]
   return sum
 
-def print_stats(list_of_draws, total_draws, number_of_alphagrams, alphagram_sum):
+def print_stats(list_of_draws, total_draws, number_of_alphagrams, alphagram_sum, alphagram_start):
+    print("For alphagrams "+str(alphagram_start)+"-"+str(alphagram_start+number_of_alphagrams)+":")
     print("Percent of total alphagrams: "+str(round((number_of_alphagrams/len(list_of_draws))*100,2))+"%")
     print("Total draws for top "+ str(number_of_alphagrams)+" alphagrams: "+str(alphagram_sum))
     print("Percent of total draws: " + str(round((alphagram_sum/total_draws)*100,2))+"%")
